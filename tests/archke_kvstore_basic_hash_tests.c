d760af61ce24de62d9684cdf29350ffe5d6f8300
gapusta <gapusta97@gmail.com>
2025-01-03 20:31:39 +0600

    Added tests

--- tests/archke_kvstore_basic_hash_tests.c
+++ tests/archke_kvstore_basic_hash_tests.c
@@ -49,15 +49,81 @@ void test1() {
 }
 
 void test2() {
+    int code = rchkKVStorePut("my_key_1", strlen("my_key_1"), "my_value_1", strlen("my_value_1"));
+    if (code < 0) {
+        printf("Test 2 failed: 'put' #1 returned error code: %d\n", code);
+        return;
+    }
+
+    code = rchkKVStorePut("my_key_2", strlen("my_key_2"), "my_value_2", strlen("my_value_2"));
+    if (code < 0) {
+        printf("Test 2 failed: 'put' #1 returned error code: %d\n", code);
+        return;
+    }
+    
+    char* value = NULL;
+    int valueSize = 0;
+
+    code = rchkKVStoreGet("my_key_1", strlen("my_key_1"), &value, &valueSize);
+    if (code == ARCHKE_KVSTORE_NOT_FOUND) {
+        printf("Test 2 failed: 'get' #1 returned 'not found' code\n");
+        return;
+    }
+    if (code < 0) {
+        printf("Test 2 failed: 'get' #1 returned error code: %d\n", code);
+        return;
+    }
+
+    if (valueSize != strlen("my_value_1")) {
+        printf("Test 2, 'get' #1 failed: sizes do not match\n");
+        return;
+    }
+
+    if (strcmp(value, "my_value_1") != 0) {
+        printf("Test 2, 'get' #1 failed: contents do not match\n");
+        return;
+    }
+
+    // print(value, valueSize);
+
+    value = NULL;
+    valueSize = 0;
+
+    code = rchkKVStoreGet("my_key_2", strlen("my_key_2"), &value, &valueSize);
+    if (code == ARCHKE_KVSTORE_NOT_FOUND) {
+        printf("Test 2 failed: 'get' #2 returned 'not found' code\n");
+        return;
+    }
+    if (code < 0) {
+        printf("Test 2 failed: 'get' #2 returned error code: %d\n", code);
+        return;
+    }
+
+    if (valueSize != strlen("my_value_2")) {
+        printf("Test 2, 'get' #2 failed: sizes do not match\n");
+        return;
+    }
+
+    if (strcmp(value, "my_value_2") != 0) {
+        printf("Test 2, 'get' #2 failed: contents do not match\n");
+        return;
+    }
+
+    // print(value, valueSize);
+
+    printf("Test 2 passed\n");
+}
+
+void test3() {
     int code = rchkKVStorePut("my_key", strlen("my_key"), "my_value_2112", strlen("my_value_2112"));
     if (code < 0) {
-        printf("Test 2 failed: 'put' returned error code: %d\n", code);
+        printf("Test 3 failed: 'put' returned error code: %d\n", code);
         return;
     }
 
     code = rchkKVStorePut("my_key", strlen("my_key"), "my_next_value_2112", strlen("my_next_value_2112"));
     if (code < 0) {
-        printf("Test 2 failed: 'put' returned error code: %d\n", code);
+        printf("Test 3 failed: 'put' returned error code: %d\n", code);
         return;
     }
     
@@ -66,27 +132,27 @@ void test2() {
 
     code = rchkKVStoreGet("my_key", strlen("my_key"), &value, &valueSize);
     if (code == ARCHKE_KVSTORE_NOT_FOUND) {
-        printf("Test 2 failed: 'get' returned 'not found' code\n");
+        printf("Test 3 failed: 'get' returned 'not found' code\n");
         return;
     }
     if (code < 0) {
-        printf("Test 2 failed: 'get' returned error code: %d\n", code);
+        printf("Test 3 failed: 'get' returned error code: %d\n", code);
         return;
     }
 
     // print(value, valueSize);
 
     if (valueSize != strlen("my_next_value_2112")) {
-        printf("Test 2 failed: sizes do not match\n");
+        printf("Test 3 failed: sizes do not match\n");
         return;
     }
 
     if (strcmp(value, "my_next_value_2112") != 0) {
-        printf("Test 2 failed: contents do not match\n");
+        printf("Test 3 failed: contents do not match\n");
         return;
     }
 
-    printf("Test 2 passed\n");
+    printf("Test 3 passed\n");
 }
 
 int main(void) {
@@ -94,6 +160,7 @@ int main(void) {
 
     test1();
     test2();
+    test3();
 
     return 0;
 }
